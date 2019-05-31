92f7672e71579d08aa598f26925b4291f3495ae2
txiong000 <tkx422@gmail.com>
2019-05-31 08:47:07 -0500

    refactor to material ui

--- market-org/src/components/SignIn.js
+++ market-org/src/components/SignIn.js
@@ -1,14 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'
+import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel, withStyles } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import { Link, withRouter } from 'react-router-dom'
 import { auth, googleProvider } from '../firebase';
 
 
-const SignIn = (props) => {
+const styles = theme => ({
+	main: {
+		width: 'auto',
+		display: 'block', // Fix IE 11 issue.
+		marginLeft: theme.spacing.unit * 3,
+		marginRight: theme.spacing.unit * 3,
+		[theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+			width: 400,
+			marginLeft: 'auto',
+			marginRight: 'auto',
+		},
+	},
+	paper: {
+		marginTop: theme.spacing.unit * 8,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+	},
+	avatar: {
+		margin: theme.spacing.unit,
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing.unit,
+	},
+	submit: {
+		marginTop: theme.spacing.unit * 3,
+	},
+});
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+function SignIn(props) {
+	const { classes } = props
 
-    
-    const signInWithGoogle = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const signInWithGoogle = () => {
         auth.signInWithPopup(googleProvider)
             .then(({user}) => {
             console.log("user:", user);
@@ -30,33 +65,48 @@ const SignIn = (props) => {
             })
      }
 
-     return (
-        <div>
-            <form autoComplete="off">
-                <input
-                    label="email"
-                    name="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                 />
-                 <input
-                    label="password"
-                    name="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                 />
-                 <button type='button' onClick={signInWithEmailAndPassword}>
-                    Sign In
-                </button>
-            </form>
-            <button type='button' onClick={signInWithGoogle}>
-                Sign In with Google
-            </button>
-        </div>
-    )
+	return (
+		<main className={classes.main}>
+			<Paper className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in
+       			</Typography>
+				<form className={classes.form} onSubmit={e => e.preventDefault() && false}>
+					<FormControl margin="normal" required fullWidth>
+						<InputLabel htmlFor="email">Email Address</InputLabel>
+						<Input id="email" name="email" autoComplete="off" autoFocus value={email} onChange={e => setEmail(e.target.value)} />
+					</FormControl>
+					<FormControl margin="normal" required fullWidth>
+						<InputLabel htmlFor="password">Password</InputLabel>
+						<Input name="password" type="password" id="password" autoComplete="off" value={password} onChange={e => setPassword(e.target.value)} />
+					</FormControl>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						onClick={signInWithEmailAndPassword}
+						className={classes.submit}>
+						Sign in
+          			</Button>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="secondary"
+						onClick={signInWithGoogle}
+						className={classes.submit}>
+						Sign in with Google
+          			</Button>
+				</form>
+			</Paper>
+		</main>
+	)
 
 }
 
-export default SignIn;
+
+export default withRouter(withStyles(styles)(SignIn));
