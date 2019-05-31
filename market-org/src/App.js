7677e00cdee70f2faaec5931cd39774fa6b9ff38
txiong000 <tkx422@gmail.com>
2019-05-30 20:23:52 -0500

    using axios get method to fetch data

--- market-org/src/App.js
+++ market-org/src/App.js
@@ -9,18 +9,17 @@ import './App.css';
 
 function App(props) {
 
-  const [users, setUsers] = useState({});
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000')
-         .then(response => {
-           console.log('response:', response)
-           return (
-              setUsers(reponse)
-           )
-           
+    axios.get('https://market-organizer.herokuapp.com/users')
+         .then(res => {
+           console.log(res)
+           setUsers(res.data)
          })
-         console.log(users)
+         .catch(err => {
+           console.log(err.message)
+         })
   }, [])
 
 
@@ -31,11 +30,17 @@ function App(props) {
   }
 
   return (
+    
     <div className="App">
       <NavLink to="/signin">Login</NavLink>
       <Route exact path="/" component={SignUp} />
       <Route path="/signin" component={SignIn} />
       <button type="button" onClick={logout}>Logout</button>
+      { users && users.map(user => {
+        return (
+          <div key={user.id}>{user.email}</div>
+        )
+      })}
     </div>
   );
 }
