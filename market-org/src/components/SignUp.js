f7cd18174d01dfbce91d09376886b86cb6b3d3b0
txiong000 <tkx422@gmail.com>
2019-05-31 08:47:36 -0500

    refactor to material-ui

--- market-org/src/components/SignUp.js
+++ market-org/src/components/SignUp.js
@@ -1,22 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'
+import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import withStyles from '@material-ui/core/styles/withStyles'
 
 import { auth, googleProvider } from '../firebase';
+import { Link, withRouter } from 'react-router-dom'
 
+const styles = theme => ({
+	main: {
+		width: 'auto',
+		display: 'block', // Fix IE 11 issue.
+		marginLeft: theme.spacing.unit * 3,
+		marginRight: theme.spacing.unit * 3,
+		[theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+			width: 400,
+			marginLeft: 'auto',
+			marginRight: 'auto',
+		},
+	},
+	paper: {
+		marginTop: theme.spacing.unit * 8,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+	},
+	avatar: {
+		margin: theme.spacing.unit,
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing.unit,
+	},
+	submit: {
+		marginTop: theme.spacing.unit * 3,
+	},
+})
 
+function Register(props) {
+	const { classes } = props
 
-const SignUp = (props) => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
 
-    // local states for signing up with email/password
-
-    const [email, setEmail] = useState(''); 
-    const [password, setPassword] = useState('');
-    const [market, setMarket] = useState(false);
-    const [vendor, setVendor] = useState(false);
-
-
-    // using google sign up authentication
-
-    const signUpWithGoogle = () => {
+	const signUpWithGoogle = () => {
         auth.signInWithPopup(googleProvider)
             .then(({user}) => {
             console.log("user:", user);
@@ -37,36 +65,51 @@ const SignUp = (props) => {
             .catch(err => {
                 console.log(err);
             })
+           
      }
 
+	return (
+		<main className={classes.main}>
+			<Paper className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Register Account
+       			</Typography>
+				<form className={classes.form} onSubmit={e => e.preventDefault() && false }>
+					<FormControl margin="normal" required fullWidth>
+						<InputLabel htmlFor="email">Email Address</InputLabel>
+						<Input id="email" name="email" autoComplete="off" value={email} onChange={e => setEmail(e.target.value)}  />
+					</FormControl>
+					<FormControl margin="normal" required fullWidth>
+						<InputLabel htmlFor="password">Password</InputLabel>
+						<Input name="password" type="password" id="password" autoComplete="off" value={password} onChange={e => setPassword(e.target.value)}  />
+					</FormControl>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						onClick={signUpWithEmailAndPassword}
+						className={classes.submit}>
+						Register
+          			</Button>
 
-    return (
-        <div>
-            <form autoComplete="off">
-                <input
-                    label="email"
-                    name="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                 />
-                 <input
-                    label="password"
-                    name="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                 />
-                 <button type='button' onClick={signUpWithEmailAndPassword}>
-                    Sign Up
-                </button>
-            </form>
-            <button type='button' onClick={signUpWithGoogle}>
-                Sign Up with Google
-            </button>
-        </div>
-    )
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="secondary"
+						onClick={signUpWithGoogle}
+						className={classes.submit}>
+						Sign up with Google
+          			</Button>
+				</form>
+			</Paper>
+		</main>
+	)
+
 }
 
-
-export default SignUp;
+export default withRouter(withStyles(styles)(Register))
