c203d4545b7437230e295acb193871e3e222c9a3
txiong000 <tkx422@gmail.com>
2019-05-31 19:15:58 -0500

    trying to figure out authstatechanged

--- market-org/src/App.js
+++ market-org/src/App.js
@@ -65,16 +65,97 @@ function App() {
 
   const [users, setUsers] = useState([]);
   const classes = useStyles();
+  // useEffect(() => {
+  //   axios.get('https://market-organizer.herokuapp.com/users')
+  //     .then(res => {
+  //       console.log(res)
+  //       setUsers(res.data)
+  //     })
+  //     .catch(err => {
+  //       console.log(err.message)
+  //     })
+  // }, [])
+
   useEffect(() => {
-    axios.get('https://market-organizer.herokuapp.com/users')
-      .then(res => {
-        console.log(res)
-        setUsers(res.data)
-      })
-      .catch(err => {
-        console.log(err.message)
-      })
+    auth.onAuthStateChanged((user) => {
+      console.log("user:", user)
+      if(user) {
+        const {uid, ra, email} = user;
+        localStorage.setItem('token', ra)
+        if(user.email) {
+          const { email } = user;
+          axios.defaults.headers.common['Authorization'] = user.ra
+    
+          axios.post('http://localhost:5000/users/register', user)
+               .then(res => {
+                 console.log("res:", res);
+                 console.log("userinthen", user)
+                 setUsers(user)
+               })
+               .catch(err => {
+                 console.log(err)
+               })
+        }
+      }
+    })
+
   }, [])
+
+
+//   export const register = (user) => dispatch => {
+//     dispatch({
+//         type: authTypes.REGISTER_START
+//     })
+//     axios.defaults.headers.common['Authorization'] = user.ra
+//     axios
+//         .post(`/api/users/register`, user )
+//         .then(res => {
+//             // console.log(res.data.user.id, 'in register action')
+//             const payload = {
+//                 ...res.data,
+//                 ...user
+//             }
+//             dispatch({
+//                 type: authTypes.REGISTER_SUCCESS,
+//                 payload: payload
+//             })
+//         })
+//         .catch(err => {
+//             dispatch({
+//                 type: authTypes.REGISTER_FAIL,
+//                 payload: err
+//             })
+//         })
+// }
+
+
+
+
+
+//   auth.onAuthStateChanged((user) => {
+//     console.log(user, 'in auth listener')
+//     if (user) {
+//       const { uid, ra, email } = user;
+//       localStorage.setItem('token', ra)
+//       if (user.email) {
+//         const { email } = user;
+//         this.props.oAuth({ firebase_id: uid, email, token: ra })
+//       }
+
+//       // this.props.emailLogin(user)
+//     }
+//     else {
+//       this.setState({
+//         users: null
+//       })
+//     }
+//   })
+
+
+
+  // const storeUser = (user) => {
+  //   axios.post('/users/register')
+  // } 
 
   return (
 
@@ -97,11 +178,11 @@ function App() {
         </div>
         
         <div  style={{textAlign: 'center', marginTop: '200px'}}>
-        {users && users.map(user => {
+        {/* {users && users.map(user => {
           return (
               <div key={user.id}>{user.email}</div>
           )
-        })}
+        })} */}
         </div>
       </Container>
     </React.Fragment>
