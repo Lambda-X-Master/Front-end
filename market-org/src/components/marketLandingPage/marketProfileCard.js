b29ca9471835f8efb6b77b6395a1db8b0bdecc1b
Francis Chen <fncis.chen@gmail.com>
2019-06-11 08:50:55 -0700

    updated market profile card w stall links

--- market-org/src/components/marketLandingPage/marketProfileCard.js
+++ market-org/src/components/marketLandingPage/marketProfileCard.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from 'styled-components'
+import { BrowserRouter as Route, NavLink, Switch } from 'react-router-dom';
+import StallsList from "../stalls/stallsList";
+import MarketLandingPage from "./marketLandingPage.js";
 
 const ProfileCard = styled.div`
     border-radius: 3px;
@@ -41,6 +44,17 @@ return (
     {props.profile.contact_last_name}
     
     {props.profile.phone_number}
+
+    {props.profile.firebase_id}
+
+    <NavLink to={`/stalls/${props.profile.firebase_id}`} key={props.profile.firebase_id}>Rent Stalls/</NavLink>
+    <Switch>
+    <Route exact path="/markets" component={MarketLandingPage} />
+    <Route path="/stalls/:id" 
+    render={props =><StallsList {...props} 
+    market_id={props.profile.firebase_id}/>}
+    />  
+    </Switch>
     </div>
     </ProfileMiniCard>
 
---
+++ market-org/src/components/stalls/stallsList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from 'styled-components'
+
+const StallsList = (props) => {
+
+    return (
+        <div className="test">
+            Test
+            test
+            test
+
+            test
+            test
+            test
+
+
+
+            {props.market_id}
+
+
+            testsss
+        </div>
+    )
+}
+
+export default StallsList;
