68c1d7a6cb8ba42f6c34159cfcea5479990e1320
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-06-12 20:59:58 -0500

    shop context  set up

---
+++ market-org/src/components/context/shop.js
@@ -0,0 +1,30 @@
+import React, { useState, useReducer }  from 'react';
+
+export const ShopContext = React.createContext({
+    stalls: [],
+    cart: [],
+    addStallToCart: stall => {},
+    removeStallFromCart: stallId => {},
+    getStallsByMarketId: marketId => {}
+});
+
+const addStallToCart = stall => {
+    
+}
+
+export const ShopProvider = ({ children }) => {
+    const stalls = []
+    const [cartState, dispatch] = useReducer(shopReducer, {cart: []})
+
+    return (
+        <ShopContext.Provider 
+            value={{
+                stalls: stalls,
+                cart:cartState.cart,
+                addStallToCart: addStallToCart,
+                removeStallFromCart: removeStallFromCart
+            }}>
+            {children}
+        </ShopContext.Provider>
+    )
+}
