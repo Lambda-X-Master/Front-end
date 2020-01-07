4eded39d721cf23bba7018ad8baf7f085c12dc9a
Laura Daugherty <lauradaugherty6@gmail.com>
2020-01-07 11:46:44 -0800

    landing page setup

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { 
     Main,
+    P,
+    Container,
 } from "./styles";
+import { Link } from "react-router-dom";
 
 
 const Landing = () => {
     return (
         <Main>
-
+          <P>
+            Welcome to Space...
+          </P>
+          <Container>
+            <Link to="/login">
+              Login
+            </Link>
+            <Link to="/register">
+              Register
+            </Link>
+          </Container>
         </Main>
     )
 }
