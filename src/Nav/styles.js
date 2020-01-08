4552ba37de555c50e7f7819ec512ac32b2474ec7
Laura Daugherty <lauradaugherty6@gmail.com>
2020-01-08 11:59:05 -0800

    basic nav styles

--- src/Nav/styles.js
+++ src/Nav/styles.js
@@ -2,6 +2,25 @@ import styled from 'styled-components'
 
 export const Main = styled.main`
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center; 
+    height: 10rem;
+    width: 1000px;
+`
+
+export const MyButton = styled.div`
+   background-color: #D4C7DC;
+   color: black
+   display:inline-block;
+   padding:0.3em 1.2em;
+   margin:0 0.1em 0.1em 0;
+   border:0.16em solid rgba(255,255,255,0);
+   border-radius:2em;
+   box-sizing: border-box;
+   text-decoration:none;
+   font-family:'Roboto',sans-serif;
+   font-weight:300;
+   text-shadow: 0 0.04em 0.04em rgba(0,0,0,0.35);
+   text-align:center;
+   transition: all 0.2s;
 `
